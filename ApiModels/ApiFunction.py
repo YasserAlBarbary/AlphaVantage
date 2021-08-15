ec3e52cc07612d5efe8cec4decd5c77774c0a699
Yasser Elbarbary <yasser.m.albarbary@gmail.com>
2021-08-15 16:59:17 +0100

    Add ApiKey and ApiFunction api models

--- ApiModels/ApiFunction.py
+++ ApiModels/ApiFunction.py
@@ -1,19 +1,32 @@
 class Function:
+    _functionList = [
+        #
+        "SYMBOL_SEARCH",
+
+        #
+        "TIME_SERIES_INTRADAY",
+        "TIME_SERIES_DAILY",
+        "TIME_SERIES_WEEKLY",
+        "TIME_SERIES_MONTHLY",
+
+        # latest price
+        "GLOBAL_QUOTE",
+
+        # Technical Indicators
+        "SMA",
+        "EMA",
+    ]
 
     def __init__(self):
         self._function = "SYMBOL_SEARCH"
-        self._functionList = [
-            "SYMBOL_SEARCH",
-            "TIME_SERIES_INTRADAY",
-            "TIME_SERIES_DAILY",
-            "TIME_SERIES_WEEKLY",
-            "TIME_SERIES_MONTHLY",
-            "GLOBAL_QUOTE",  # latest price
-            "SMA",  # Technical Indicators
-        ]
+
+    def get_function(self):
+        return self._function
 
     def set_function(self, function_name):
         if function_name in self._functionList:
             self._function = function_name
         else:
             raise ValueError("Function not found.")
+
+
---
+++ ApiModels/ApiKey.py
@@ -0,0 +1,10 @@
+class Session:
+
+    def __init__(self):
+        self._api_key = "demo"
+
+    def set_api_key(self, api_key):
+        self._api_key = api_key
+
+    def get_api_ke(self):
+        return self._api_key
